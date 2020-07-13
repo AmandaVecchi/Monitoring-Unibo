f619e8fe0d5cb309a6d2f1a42becb4a264217235
AmandaVecchi <62661737+AmandaVecchi@users.noreply.github.com>
2020-07-13 13:03:37 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -1,85 +1,106 @@
-# R code for spatial view of points
+## R code for spatial view of points
 
-# show the points in a map
+#Use the spatial library in order to show the points in a map
 library(sp)
 
+#Declare the database we are going to use
 data(meuse)
 
-#Look at the data. part of the data, the first 6 lines
+#head = shows the first six lines of our database
 head(meuse)
 
-# this is a spatial dataset. Tell R this dataset contains coordinates
-coordinates(meuse) = ~ x+y    # = ~ specify which are the coordinates, firstly we have to specify the dataset that contains the coordinates
+#coordinates = sets spatial coordinates. We are declaring R this is a spatial dataset that contains coordinates
+# ~ specifies which are the coordinates
+coordinates(meuse) = ~ x+y   
 
-plot(meuse) # position of coordinates
+#plot = function for plotting objects
+plot(meuse)
 
-# plot the spatial amount of zinc
-spplot(meuse, "zinc") # declare the set and declare the variable
+#ssplot = plots several layers with a single legend for all maps
+#In this case we are plotting the spatial amount of zinc
+spplot(meuse, "zinc") #declare the set and declare the variable
 
-# plot the spatial amount of copper 
-spplot(meuse, "copper", main="copper concentration") #change title --> main="..."
+#Plot the spatial amount of copper 
+#main = titles the plot
+spplot(meuse, "copper", main="copper concentration")
 
-# show the coordinates in bubbles with different size depending on the concentration in that point
+#bubble = creates a bubble plot of spatial data
+#Each bubble is a coordinate and the size depends on the concentration in that point. Bigger the bubble, higher the concentration
 bubble(meuse, "zinc")
 bubble(meuse, "zinc", main= "Zinc concentration")
 
-#bubble copper in red
+#Pplot the copper concentration with a bubble plot in which the bubbles are colored in red
 bubble(meuse, "copper", col="red")
 bubble(meuse, "copper", main= "Copper concentration", col= "red")
 
-# take data from another source and upload them into R. --> download covid.agg
-#Put covid file into the folder LAB in our computer
+## Analysis of data regarding COVID-19
+#Download a new database called "covid.agg" and put the file into the folder "LAB" in our computer
 
-#setting the working directory: LAB
-#windows: 
+#Setting the working directory: LAB
 setwd("C:/LAB/")
-mac:
-setwd("/users/nameofthecomputer/LAB")
 
-#give a name to the dataset = covid. link the object (our dataset) to function.
-covid <- read.table("covid_agg.csv", head=TRUE) # use quotes because it is a table outside of R. head=TRUE or head=T to tell R that the first line is the header of the table
+#read.table = reads a file in table format and creates a data frame from it
+#arrow = give a name to the dataset
+#quotes are used for each object that is outside of R and in getting imported.
+covid <- read.table("covid_agg.csv", head=TRUE)
+# head=TRUE or head=T tells R that the first line is the header of the table
+
+#Show the first lines of our new dataset
 head(covid)
 
-#spatial plot --> number of cases per country
-#attach dataset to R 
+#attach = attaches the databae to the search path. Objects in the database can be accessed by simply giving their names
 attach(covid)
-plot(country,cases)
+plot(country,cases) #plot the number of cases per country
 
-#if dataset is not attach --> plot(covid$country,covid$cases)
+##If the dataset is not attaced we use $ to refer to a specific object relative to a specific data frame
+##plot(covid$country, covid$cases)
 
-#las = how to direction lables of the axis
-plot(country, cases, las=0) #parallel lables
-plot(country, cases, las=1) #horizontal lables
-plot(country, cases, las=2) #perpendicular lables
-plot(country, cases, las=3) #vertical lables
+#las = numeric value indicating the orientation of the labels and any other text added to a plot 
+plot(country, cases, las=0) #lables parallel to axis
+plot(country, cases, las=1) #lables horizontal to axis
+plot(country, cases, las=2) #lables perpendicular to axis
+plot(country, cases, las=3) #lables vertical to axis
 
-#reduce sixe of characters of axis  
-plot(country, cases, las=3, cex.axis=0.5)
-plot(country, cases, las=3, cex.axis=0.7)
+#cex = number indicating the amount by which plotting text and symbols should be scaled relative to the default
+#1 = default
+#1.5 is 50% larger
+#0.5 is 50% smaller
+#cex.axis	= changes the size of the axis annotation
+plot(country, cases, las=3, cex.axis=0.5) #reducing size of 50%
+plot(country, cases, las=3, cex.axis=0.7) #reducing size of 70%
 
-#install ggplot2 package
+########
+
+#Install "ggplot2" package
+#ggplot2 is a system for creating graphics
 install.packages("ggplot2")
-library(ggplot2)
+library(ggplot2) #in order to make use of the package
 
-#load previously saver R data
 #setting woring directory
-setwd("C/LAB/)
-load(".RData") # put the name of the workspace saved
-ls() 
+#Load previously saved R data
+setwd("C:/LAB/)
+load("First.RData") 
+#First.RData is the name of the saved working space
 
-#use ggplot2 package
+#ls = shows what data sets and functions we defined and used in the recalled workingspace
+ls() 
 library(ggplot2)
 
 data(mpg)
 head(mpg) 
-#key components og ggplot2: data, aestetics, geometry #aes = variables, geometry is separated
-ggplot(mpg, aes(x=displ,y=hwy)) + geom_point()
-ggplot(mpg, aes(x=displ,y=hwy)) + geom_line() #change the geometry, to bars, lines 
-ggplot(mpg, aes(x=displ,y=hwy)) + geom_polygon()
+
+#key components of ggplot2: data, aesthetics, geometry 
+#aes = declare the variables
+#geom_point() = declare the geometry of the plot 
+ggplot(mpg, aes(x=displ,y=hwy)) + geom_point() 
+ggplot(mpg, aes(x=displ,y=hwy)) + geom_line() #change the geometry to lines 
+ggplot(mpg, aes(x=displ,y=hwy)) + geom_polygon() #change the geometry to polygons
 
 #ggplot for our covid dataset
 head(covid)
-ggplot(covid, aes(x=lon, y=lat, size=cases)) + geom_point() #size =dimension of point changes with respect to a variable
+ggplot(covid, aes(x=lon, y=lat, size=cases)) + geom_point() 
+#size = x -->  dimension of points changes with respect to a variable x
+#In this case the size of point changes accordingly to the number of cases
 
 
 
