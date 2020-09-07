fc0884c5e824a8ab52ef005e093cfdbf0d3cedb4
AmandaVecchi <62661737+AmandaVecchi@users.noreply.github.com>
2020-09-07 22:23:02 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -1,92 +1,71 @@
 ## R code for spatial view of points
 
-#Use the spatial library in order to show the points in a map
+#Recalling the previously intalled package to import, manipulate and export spatial data
 library(sp)
-
-#Declare the database we are going to use
 data(meuse)
-
-#head = shows the first six lines of our database
 head(meuse)
 
-#coordinates = sets spatial coordinates. We are declaring R this is a spatial dataset that contains coordinates
-# ~ specifies which are the coordinates
-coordinates(meuse) = ~ x+y   
-
-#plot = function for plotting objects
+#The "coordinates" function estimates spatial coordinates for the elements in the dataset.
+#We are declaring R this is a spatial dataset that contains points related to specific coordinates
+coordinates(meuse) = ~ x+y #The tilde operator specifies which are the coordinates
 plot(meuse)
 
-#spplot = plots several layers with a single legend for all maps
+#The "spplot" function allows us to plot several layers with a single legend for all maps
 #In this case we are plotting the spatial amount of zinc
-spplot(meuse, "zinc") #declare the set and declare the variable
+spplot(meuse, "zinc") #In the argument we declare the dataset and the variable we are plotting
+spplot(meuse, "copper", main="copper concentration") #Main allows us to title the plot we create
 
-#Plot the spatial amount of copper 
-#main = titles the plot
-spplot(meuse, "copper", main="copper concentration")
-
-#bubble = creates a bubble plot of spatial data
-#Each bubble is a coordinate and the size depends on the concentration in that point. Bigger the bubble, higher the concentration
+#The "bubble" function creates a bubble plot of spatial data
+#Each bubble represent a coordinate and a variable; the size of the bubble depends on the concentration in that point. Bigger the bubble, higher the concentration
 bubble(meuse, "zinc")
 bubble(meuse, "zinc", main= "Zinc concentration")
 
-#Pplot the copper concentration with a bubble plot in which the bubbles are colored in red
 bubble(meuse, "copper", col="red")
 bubble(meuse, "copper", main= "Copper concentration", col= "red")
+
 
 ## Analysis of data regarding COVID-19
 #Download a new database called "covid.agg" and put the file into the folder "LAB" in our computer
 
-#Setting the working directory: LAB
+#Setting the working directory (where we put the images or tables we are using for our analysis): LAB
 setwd("C:/LAB/")
 
-#read.table = reads a file in table format and creates a data frame from it
-#arrow = give a name to the dataset
+#The "read.table" function reads a file in table format and creates a data frame from it. Rows correspond to cases and columns to variables
+#<- is an operator that assigns a name to the function we perform
+covid <- read.table("covid_agg.csv", head=TRUE) 
 #quotes are used for each object that is outside of R and in getting imported.
-covid <- read.table("covid_agg.csv", head=TRUE)
-# head=TRUE or head=T tells R that the first line is the header of the table
+#head=TRUE or head=T tells R that the first line in the table is the header of the table
 
-#Show the first lines of our new dataset
 head(covid)
-
-#attach = attaches the databae to the search path. Objects in the database can be accessed by simply giving their names
 attach(covid)
-plot(country,cases) #plot the number of cases per country
+plot(country,cases) #We are plotting the number of cases per country
 
 ##If the dataset is not attaced we use $ to refer to a specific object relative to a specific data frame
 ##plot(covid$country, covid$cases)
 
-#las = numeric value indicating the orientation of the labels and any other text added to a plot 
+#las is a numeric value indicating the orientation of the labels and any other text added to a plot 
 plot(country, cases, las=0) #lables parallel to axis
 plot(country, cases, las=1) #lables horizontal to axis
 plot(country, cases, las=2) #lables perpendicular to axis
 plot(country, cases, las=3) #lables vertical to axis
-
-#cex = number indicating the amount by which plotting text and symbols should be scaled relative to the default
-#1 = default
-#1.5 is 50% larger
-#0.5 is 50% smaller
-#cex.axis	= changes the size of the axis annotation
 plot(country, cases, las=3, cex.axis=0.5) #reducing size of 50%
 plot(country, cases, las=3, cex.axis=0.7) #reducing size of 70%
 
 ########
 
-#Install "ggplot2" package
-#ggplot2 is a system for creating graphics
+#Install and stating we are making use of the "ggplot2" package
+#The ggplot2 package allows us to plot variables 
 install.packages("ggplot2")
-library(ggplot2) #in order to make use of the package
+library(ggplot2) 
 
-#setting woring directory
-#Load previously saved R data
+#Setting woring directory: LAB
+#The "load" function allows us to upload previously saved R datasets
 setwd("C:/LAB/")
-load("First.RData")
-#In brackets there is the name of the saved working space
+load("First.RData") #In brackets there is the name of the saved working space
 
-#ls = shows what data sets and functions we defined and used in the recalled workingspace
+#The "ls" function shows what datasets and functions we defined and used in the recalled working space
 ls() 
-library(ggplot2)
-
-data(mpg)
+data(mpg) #We are using a new dataset
 head(mpg) 
 
 #Key components of ggplot2: data, aesthetics, geometry 
@@ -96,10 +75,10 @@ ggplot(mpg, aes(x=displ,y=hwy)) + geom_point()
 ggplot(mpg, aes(x=displ,y=hwy)) + geom_line() #change the geometry to lines 
 ggplot(mpg, aes(x=displ,y=hwy)) + geom_polygon() #change the geometry to polygons
 
-#ggplot for our covid dataset
+#Let's create a ggplot for our covid dataset
 head(covid)
 ggplot(covid, aes(x=lon, y=lat, size=cases)) + geom_point() 
-#size = x -->  dimension of points changes with respect to a variable, x
+#size = cases -->  dimension of points changes with respect to the variable we stated
 #In this case the size of points changes accordingly to the number of cases
 
 
