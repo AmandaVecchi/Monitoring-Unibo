628943c7fe5bf47d38e4278296fa6646f6342d62
AmandaVecchi <62661737+AmandaVecchi@users.noreply.github.com>
2020-09-07 15:45:01 +0200

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -1,20 +1,31 @@
+## Our first R code
+
+#Installing a package that allows us to import, manipulate and export spatial data 
 install.packages("sp")
-
 library(sp)
-data(meuse)
 
-#Let's see how the meuse dataset is structured:
+#The "data" function allows us to upload specified data sets
+data(meuse) #meuse is a dataset containing locations and topsoil heavy metal concentrations
+
+#Let's see how the meuse dataset is structured
 meuse
 
-#Let's look at the first row of the set. Only see the first part of the dataset, six lines
+#The "head" function allows us to see the first six rows of the data set we are using
 head(meuse)
 
 #Let's plot two variables together to see if they are related
-#See if zinc concentration is related to that of copper --> attach a dataset to R first, then plot the two variables
+#See if zinc concentration is related to that of copper 
+
+#The "attach" function connects the database to the R search path. With this function the objects (in this case heavy metal concentrations) in the database can be accessed by giving their names
 attach(meuse)
+
+#The "plot" function plots on a generic x;y graph two different variables
 plot(zinc, copper)
-plot(zinc, copper,col="green")
-#change symbol of plot --> pch (=symbol) and the number of the symbol
-plot(zinc, copper,col="green",pch=19)
-#make bigger point --> cex = character exhageration --> 2= double the symbol. To decrease put number between 0 and 1 
-plot(zinc, copper,col="green",pch=19,cex=2)
+plot(zinc, copper,col="green") #col = argument to state in which colour to plot the points in the graph
+plot(zinc, copper,col="green",pch=19) #pch = argument used to specify point shapes
+plot(zinc, copper,col="green",pch=19,cex=2) #cex = argument used to specify character exaggeration 
+
+#cex = 0.5 diminishes the size of the point of 50%
+#cex= 2 enhances the size of the point of 200%
+#cex = 0.7 decreases the seize of the poin of 70%
+#in general to mininish the size of the pont we put 0 < cex < 1
