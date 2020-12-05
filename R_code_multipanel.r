28032eb5175b5bbe870a021b4766496c80eda02b
AmandaVecchi <62661737+AmandaVecchi@users.noreply.github.com>
2020-12-05 16:58:32 +0100

    Update R_code_multipanel.r

--- R_code_multipanel.r
+++ R_code_multipanel.r
@@ -1,10 +1,10 @@
-## Multipanel in R: second lecture of Monitoring Ecosystems
+## Multipanel in R
 
 install.packages("sp")
 install.packages("GGally")
 #GGally is an extension of ggplot2; it adds functions to reduce the complexity of combining geometric objects with transformed data
 
-library(sp) #require(sp) will also do the job
+library(sp) #require(sp) 
 library(GGally)
 
 data(meuse) 
@@ -29,13 +29,14 @@ pairs(meuse) #creates all the possible pairwise plots of the dataset
 #Create pairs scatterplots considering only some variables:cadmium, copper, lead, zinc
 #The tilde operator states which are the dependent variables
 pairs(~ cadmium + copper + lead + zinc, data=meuse) #in this case we also state the dataset in which the variables are contained
-pairs(meuse[,3:6]) #using the square brackets tell R to start take all variables starting from column number 3 to column number 6 
+pairs(meuse[,3:6]) #using the square brackets tell R to start take all variables starting from column number 3 to column number 6 in the dataset we are using
 
 #Let's prettify this graph
 pairs(meuse[,3:6], pch=15,col="blue") 
 pairs(meuse[,3:6], pch=15,col="blue", cex=0.5) #cex = 0.5 diminishes point size of 50%
 
 #Now let's add info to the scatter plot with the GGally package (install a new package, but at beginning of code)
+#The ggpairs() function of the GGally package allows to build a scatterplot matrix
 ggpairs(meuse[,3:6])
 
 
