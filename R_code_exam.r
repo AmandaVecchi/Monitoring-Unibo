00932bea971ee3498ea11cae00c931e39e6dd986
AmandaVecchi <62661737+AmandaVecchi@users.noreply.github.com>
2021-09-06 17:04:56 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -965,22 +965,115 @@ plot(s1, col=cl)
     
 ########## 17. R code exam project
     
+insall.packages(“raster”)
+install.packages(“rasterVis”)
+install.packages(“rasterdiv”)
+install.packages(“rgdal”)
+install.packages(“ggplot2”)
+install.packages(“RStoolbox”)
     
+library(raster)
+library(rasterVis)
+library(rasterdiv)
+library(rgdal)
+library(ggplot2)
+library(RStoolbox)
     
+#Upload image from 2018
+setwd(“C:/EXAM/2018DATA/”)
+rlist2018 <- list.files( pattern = "2018")
+rlist2018
+
+#B2 = BLUE
+#B3 = GREEN
+#B4 = RED
+#B5 = NIR
+#B6 = SWIR1
+#B7 = SWIR2
+
+import_image2018 <- lapply(rlist2018, raster)
+image2018 <- stack(import_image2018)
+image2018
+
+RGB2018 <- ggRGB(image2018, r=4, g=3, b=2, stretch ="Lin")
+plot(RGB2018)
     
+visible2018 <- print(RGB2018 + ggtitle("RGB map - Sardinia, 2018")+ theme (axis.title.x = element_blank(),axis.title.y = element_blank()))    
     
+setwd(“C:/EXAM/2021DATA/”)
+rlist2021 <- list.files(pattern = "2021")
+rlist2021
     
+import_image2021 <- lapply(rlist2021, raster)
+image2021 <- stack(import_image2021)
+image2021
+
+RGB2018 <- ggRGB(image2021, r=4, g=3, b=2, stretch ="Lin")
+plot(RGB2021) 
     
-    
-    
+visible2021_print <- print(RGB2021 + ggtitle("RGB map - Sardinia, 2021")+ theme (axis.title.x = element_blank(),axis.title.y = element_blank()))    
 
+#plot both RGB images together
+par(mfrow = c(1,2) , oma=c(0,0,2,0))
+plotRBG(image2018,  r=4, g=3, b=2, stretch =”lin”)
+plotRGB(image2021, r=4, g=3, b=2, stretch =”lin”)
+mtext(“Visible RGB Sardinia 2018 vs Sardinia 2021”, outer = TRUE, cex = 1.5)
 
+par(mfrow = c(1,2), oma = c(0,0,2,0))
+plotRBG(image2018,  r=4, g=3, b=2, stretch =”hist”)
+plotRGB(image2021, r=4, g=3, b=2, stretch =”hist”)
+mtext(“Visible RGB Sardinia 2018 vs Sardinia 2021”, outer=TRUE, cex =1.5)
 
+#CALCULATE NDVI
+#NDVI = (NIR – RED) / (NIR+RED)
+NDVI2018 <- (image2018[[5]] - image2018[[4]]) / (image2018[[5]] + image2018[[4]])
+NDVI2018
+Plot(NDVI2018, main = "NDVI 2018")
 
+NDVI2021 <- (image2021[[5]] - image2021[[4]]) / (image2021[[5]] + image2021[[4]])
+NDVI2021
+Plot(NDVI2021, main = "NDVI 2021")
 
+#PLOT BOTH NDVI IMAGES TOGETHER
+par(mfrow = c(1,2), oma=c(0,0,2,0))
+plot(NDVI2018, main = "NDVI 2018") 
+plot(NDVI2021, main = "NDVI 2021") 
+mtext(“NDVI comparison: 2018 vs 2021”, outer=TRUE, cex =1.5)
 
+#difference between the two years
+diffNDVI <- (NDVI2018 – NDVI2021)
+plot(diffNDVI)
+plot(diffNDVI, main = “NDVI difference between 2018 and 2021”)
 
+cld <- colorRampPalette(c('blue','white','red'))(100)
+plot(diffNDVI, col = cld, main = "NDVI difference between 2018 and 2021")
 
+#VISUALIZATION USING HISTOGRAMS
+hist_NDVI2018 <- hist(NDVI2018, main = "Distribution of NDVI values - 2018", xlab = "NDVI", ylab = "Frequency", breaks = 30)
+hist_NDVI2021 <- hist(NDVI2021, main = "Distribution of NDVI values - 2021", xlab = "NDVI", ylab = "Frequency", breaks = 30)
 
+par(mfrow = c(1,2), oma=c(0,0,2,0))
+plot(hist_NDVI2018, main = “NDVI 2018”) 
+plot(hist_NDVI2021, main = “NDVI 2021”) 
+mtext(“Comparison of distribution of NDVI values: 2018 vs 2021”, outer=TRUE, cex =1.5)
 
+#calculate NBR = Normalized Burned Ratio. an index designed to highlight burnt areas 
+#NBR = (NIR-SWIR) / (NIR+SWIR)
+#A high NBR value indicates healthy vegetation while a low value indicates bare ground and recently burnt areas. Non-burnt areas are normally attributed to values close to zero
+
+#2018 NBR
+setwd(“C:/EXAM/2018DATA/”)
+getwd()
+NBR_2018 <- (image2018[[5]] - image2018[[6]]) / (image2018[[5]] + image2018[[6]])
+plot(NBR_2018, main = “NBR values for 2018”)
+
+#2021 NBR
+setwd(“C:/EXAM/2021DATA/”)
+getwd()
+NBR_2021 <- (image2021[[5]] - image2021[[6]]) / (image2021[[5]] + image2021[[6]])
+plot(NBR_2021, main = “NBR values for 2021)
+
+#dNBR shows difference between NBR of the two years
+dNBR <- (NBR_2018 – NBR_2021)
+Plot(dNBR, main = “Difference in NBR 2018 – 2021”) 
 
