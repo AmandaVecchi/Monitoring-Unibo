63bf0d4e60ee8afacc9db3b5da0b23a45647aaa4
AmandaVecchi <62661737+AmandaVecchi@users.noreply.github.com>
2021-09-08 08:25:35 +0200

    Update project_new.R

--- project_new.R
+++ project_new.R
@@ -4,8 +4,7 @@ install.packages("rasterdiv")
 install.packages("rgdal")
 install.packages("ggplot2")
 install.packages("RStoolbox")
-install.packages("rgeos")
-install.packages("devtools")
+install.packages("gridExtra")
     
 library(raster)
 library(rasterVis)
@@ -13,13 +12,12 @@ library(rasterdiv)
 library(rgdal)
 library(ggplot2)
 library(RStoolbox)
-library(rgeos)
-library(devtools)
+library(gridExtra)
     
-#Upload image from 2014
-setwd("C:/EXAM/2014DATA/")
-rlist2014 <- list.files( pattern = "2014")
-rlist2014
+#Upload image from 2013
+setwd("C:/exam/2018/")
+rlist2018 <- list.files( pattern = "2018")
+rlist2018
 
 #B2 = BLUE
 #B3 = GREEN
@@ -28,23 +26,23 @@ rlist2014
 #B6 = SWIR1
 #B7 = SWIR2
 
-import_image2014 <- lapply(rlist2014, raster)
-image2014 <- stack(import_image2014)
-image2014
+import_image2018 <- lapply(rlist2018, raster)
+image2018 <- stack(import_image2018)
+image2018
 
 #crop the image
-ext <- c(930000, 1050000, 4800000,4900000)
-zoom(image2014, ext=ext)
-image2014_crop <- crop(image2014, ext)
-plot(image2014_crop)
+ext <- c(430000, 525000, 4375000, 4475000)
+zoom(image2018, ext=ext)
+image2018_crop <- crop(image2018, ext)
+plot(image2018_crop)
 
 #plot the image with RGB colours 
-RGB2014 <- ggRGB(image2014_crop, r=4, g=3, b=2, stretch ="Lin")
-plot(RGB2014)
+RGB2018 <- ggRGB(image2018_crop, r=3, g=2, b=1, stretch ="Lin")
+plot(RGB2018)
     
-visible2014 <- print(RGB2014 + ggtitle("Visible - Sardinia, 2014")+ theme (axis.title.x = element_blank(),axis.title.y = element_blank()))    
+visible2018 <- print(RGB2018 + ggtitle("Visible - Sardinia, 2018")+ theme (axis.title.x = element_blank(),axis.title.y = element_blank()))    
     
-setwd("C:/project/2021/")
+setwd("C:/exam/2021/")
 rlist2021 <- list.files(pattern = "2021")
 rlist2021
     
@@ -53,7 +51,7 @@ image2021 <- stack(import_image2021)
 image2021
 
 #crop the image
-ext <- c(930000, 1050000, 4800000,4900000)
+ext <- c(430000, 525000, 4375000, 4475000)
 zoom(image2021, ext=ext)
 image2021_crop <- crop(image2021, ext)
 plot(image2021_crop)
@@ -61,37 +59,39 @@ plot(image2021_crop)
 RGB2021 <- ggRGB(image2021_crop, r=4, g=3, b=2, stretch ="Lin")
 plot(RGB2021) 
     
-visible2021_print <- print(RGB2021 + ggtitle("Visible - Sardinia, 2021")+ theme (axis.title.x = element_blank(),axis.title.y = element_blank()))    
+visible2021 <- print(RGB2021 + ggtitle("Visible - Sardinia, 2021")+ theme (axis.title.x = element_blank(),axis.title.y = element_blank()))    
+
+grid.arrange(visible2018, visible2021, nrow =1)
 
 #plot both RGB images together
 par(mfrow = c(1,2) , oma=c(0,0,2,0))
-plotRBG(image2018,  r=4, g=3, b=2, stretch =”lin”)
-plotRGB(image2021, r=4, g=3, b=2, stretch =”lin”)
-mtext(“Visible RGB Sardinia 2018 vs Sardinia 2021”, outer = TRUE, cex = 1.5)
+plotRGB(image2018_crop, r=4, g=3, b=2, stretch ="lin")
+plotRGB(image2021_crop, r=4, g=3, b=2, stretch ="lin")
+mtext("Visible Sardinia 2018 vs Sardinia 2021", outer = TRUE, cex = 1.5)
 
 par(mfrow = c(1,2), oma = c(0,0,2,0))
-plotRBG(image2018,  r=4, g=3, b=2, stretch =”hist”)
-plotRGB(image2021, r=4, g=3, b=2, stretch =”hist”)
-mtext(“Visible RGB Sardinia 2018 vs Sardinia 2021”, outer=TRUE, cex =1.5)
+plotRGB(image2018_crop,  r=4, g=3, b=2, stretch ="hist")
+plotRGB(image2021_crop, r=4, g=3, b=2, stretch ="hist")
+mtext("Visible RGB Sardinia 2020 vs Sardinia 2021", outer=TRUE, cex =1.5)
 
 #CALCULATE NDVI
 #NDVI = (NIR – RED) / (NIR+RED)
-NDVI2018 <- (image2018[[5]] - image2018[[4]]) / (image2018[[5]] + image2018[[4]])
+NDVI2018 <- (image2018_crop[[5]] - image2018_crop[[4]]) / (image2018_crop[[5]] + image2018_crop[[4]])
 NDVI2018
-Plot(NDVI2018, main = "NDVI 2018")
+plot(NDVI2018, main = "NDVI - Sardinia, 2018")
 
-NDVI2021 <- (image2021[[5]] - image2021[[4]]) / (image2021[[5]] + image2021[[4]])
+NDVI2021 <- (image2021_crop[[5]] - image2021_crop[[4]]) / (image2021_crop[[5]] + image2021_crop[[4]])
 NDVI2021
-Plot(NDVI2021, main = "NDVI 2021")
+plot(NDVI2021, main = "NDVI 2021")
 
 #PLOT BOTH NDVI IMAGES TOGETHER
 par(mfrow = c(1,2), oma=c(0,0,2,0))
-plot(NDVI2018, main = "NDVI 2018") 
+plot(NDVI2020, main = "NDVI 2020") 
 plot(NDVI2021, main = "NDVI 2021") 
-mtext(“NDVI comparison: 2018 vs 2021”, outer=TRUE, cex =1.5)
+mtext(“NDVI comparison: 2020 vs 2021”, outer=TRUE, cex =1.5)
 
 #Difference between the two years
-diffNDVI <- (NDVI2018 – NDVI2021)
+diffNDVI <- (NDVI2020 - NDVI2021)
 plot(diffNDVI)
 plot(diffNDVI, main = “NDVI difference between 2018 and 2021”)
 
